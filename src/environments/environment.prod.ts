85625bb30e1d63a3a007fa0e89240d36dd4f0d87
Sanjayvenghat A T <sanjayvenghat9@gmail.com>
2026-06-14 21:10:33 +0530

    changeurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  LoginUrl: "http://localhost:3000",
+  LoginUrl: "https://backend-seven-pi-49.vercel.app",
   EncryptionKey: "pW6zA1qD9KoXz3cY"
 };
